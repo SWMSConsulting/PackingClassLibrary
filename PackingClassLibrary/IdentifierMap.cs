4379c46d587ca53099bd222b8082d568cfc0d1d0
ArnoSchiller <arno-schiller@hotmail.de>
2024-01-11 09:09:28 +0100

    added a service to handle identifier mapping, including CRU via MQTT

---
+++ PackingClassLibrary/IdentifierMap.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace PackingClassLibrary
+{
+    public class IdentifierMap : Serializable
+    {
+        [JsonProperty("article_id", Required = Required.AllowNull)]
+        public string ArticleId { get; set; }
+
+
+        [JsonProperty("grundner_id", Required = Required.AllowNull)]
+        public int? GrundnerId { get; set; }
+
+        [JsonProperty("message", Required = Required.Default)]
+        public string? Message { get; set; } = null;
+    }
+}
