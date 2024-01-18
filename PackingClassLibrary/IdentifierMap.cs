2e99a19204c946773d0715d83a424f7e98b529c9
ArnoSchiller <arno-schiller@hotmail.de>
2024-01-18 11:49:23 +0100

    Added AutomationType to add new articles to the grundner system

--- PackingClassLibrary/IdentifierMap.cs
+++ PackingClassLibrary/IdentifierMap.cs
@@ -16,17 +16,14 @@ namespace PackingClassLibrary
     {
         [JsonProperty("task", Required = Required.Always)]
         public CrudTask Task { get; set; }
-        
 
         [JsonProperty("article_id", Required = Required.AllowNull)]
-        [JsonConverter(typeof(StringEnumConverter))]
-        public string ArticleId { get; set; }
-
+        public string? ArticleId { get; set; }
 
         [JsonProperty("grundner_id", Required = Required.AllowNull)]
         public int? GrundnerId { get; set; }
 
         [JsonProperty("message", Required = Required.Default)]
-        public string? Message { get; set; } = null;
+        public string? Message { get; set; }
     }
 }
