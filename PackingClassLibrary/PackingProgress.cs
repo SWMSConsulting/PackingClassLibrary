7ec648b6b25266c5bdd5c4b949af2e8bebc112d3
ArnoSchiller <arno-schiller@hotmail.de>
2024-03-26 15:26:18 +0100

    included packing progress

---
+++ PackingClassLibrary/PackingProgress.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace PackingClassLibrary
+{
+
+    public class PackingProgress : Serializable
+    {
+        [JsonProperty("order_id", Required = Required.Always)]
+        public string OrderId { get; set; }
+
+        [JsonProperty("pallet_index", Required = Required.Always)]
+        public int PalletIndex { get; set; }
+
+        [JsonProperty("packed_articles", Required = Required.Always)]
+        public List<PackingProgressArticle> PackedArticles { get; set; }
+    }
+
+    public class PackingProgressArticle : Serializable
+    {
+        [JsonProperty("article_identifier", Required = Required.Always)]
+        public string ArticleIdentifier { get; set; }
+
+        [JsonProperty("quantity", Required = Required.Always)]
+        public int Quantity { get; set; }
+    }
+}
