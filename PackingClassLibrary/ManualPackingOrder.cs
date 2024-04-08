4d8443fe919ea6511a93711839230800e46459fd
ArnoSchiller <arno-schiller@hotmail.de>
2024-04-08 13:17:25 +0200

    added manual packing order

---
+++ PackingClassLibrary/ManualPackingOrder.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace PackingClassLibrary
+{
+    public class ManualPackingOrder : Serializable
+    {
+        [JsonProperty("order_id", Required = Required.Always)]
+        public string OrderId { get; set; }
+
+        [JsonProperty("articles", Required = Required.Always)]
+        public List<ManualPackingArticle> Articles { get; set; }
+
+
+    }
+
+    public class ManualPackingArticle : Serializable
+    {
+        [JsonProperty("article_id", Required = Required.Always)]
+        public string ArticleId { get; set; }
+
+        [JsonProperty("description", Required = Required.Always)]
+        public string Description { get; set; }
+
+        [JsonProperty("amount", Required = Required.Always)]
+        public int Amount { get; set; }
+
+    }
+}
