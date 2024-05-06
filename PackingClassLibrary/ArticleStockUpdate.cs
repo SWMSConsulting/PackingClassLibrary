eed1a073f9078d7905577e145053feecedb0f2d4
ArnoSchiller <arno-schiller@hotmail.de>
2024-05-06 16:42:15 +0200

    added article stock update

---
+++ PackingClassLibrary/ArticleStockUpdate.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace PackingClassLibrary
+{
+    public class ArticleStockUpdate: Serializable
+    {
+        [JsonProperty("article_number", Required = Required.Always)]
+        public string ArticleNumber { get; set; }
+
+        [JsonProperty("available_stock", Required = Required.Always)]
+        public int AvailableStock { get; set; }
+    }
+}
